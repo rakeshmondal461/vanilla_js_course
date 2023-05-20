e74b924bd8865cd12667c7c0c36f3296d5b2d142
rakeshmondal461 <mondalrakesh461@gmail.com>
2023-05-20 12:21:24 +0530

    code optimization

--- guessNumber.js
+++ guessNumber.js
@@ -1,38 +1,44 @@
 'use strict';
 
 let score = 20;
+let highScore = 0;
 document.querySelector('.score').textContent = score;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('#checkBtn').addEventListener('click', () => {
   const inputValue = document.querySelector('#numberField').value;
   if (!inputValue) {
-    document.querySelector('.message').textContent = '⛔ No number';
+    setMessage('⛔ No number');
   } else if (parseInt(inputValue) === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number';
+    setMessage('🎉 Correct Number');
     document.querySelector('body').style.background = '#1caf0c';
     document.querySelector('.number').style.width = '12rem';
     document.querySelector('.number').textContent = secretNumber;
-  } else if (inputValue > secretNumber) {
-    document.querySelector('.message').textContent = '📈 It is higher.';
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game.';
-      document.querySelector('.score').textContent = 0;
+    if (score > highScore) {
+      highScore = score;
     }
-  } else if (inputValue < secretNumber) {
-    document.querySelector('.message').textContent = '📉 It is lower.';
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game.';
-      document.querySelector('.score').textContent = 0;
-    }
+    document.querySelector('.highscore').textContent = highScore;
+  } else if (parseInt(inputValue) !== secretNumber) {
+    reduceScoreOnWrongGuess(inputValue);
   }
 });
+
+function reduceScoreOnWrongGuess(inputValue) {
+  setMessage(
+    inputValue > secretNumber ? '📈 It is higher.' : '📉 It is lower.'
+  );
+  if (score > 1) {
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    setMessage('💥 You lost the game.');
+    document.querySelector('.score').textContent = 0;
+  }
+}
+
+const setMessage = (message) => {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
